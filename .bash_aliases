c3cf49c459d647d7505f90b6dcea66b6acc7965b
Ethan Warrick <ewboy12@gmail.com>
2025-08-29 11:54:27 -0500

    bash,fish: improve USB speed alias

--- .bash_aliases
+++ .bash_aliases
@@ -78,3 +78,17 @@ git() {
 		command git "$@"
 	fi
 }
+
+lsusb-speed() {
+  lsusb -v 2>/dev/null | awk '
+    /^Bus/ { name=$0 }
+    /bcdUSB/ {
+      usb=$0
+      sub(/.*bcdUSB[[:space:]]+/,"",usb)
+      sub(/^Bus [0-9]+ Device [0-9]+: ID [0-9a-f]+:[0-9a-f]+ /,"",name)
+      sub(/[[:space:]]+$/, "", name)
+      if (length(name) > 68) name=substr(name,1,65)"…"
+      printf "%-68s %s\n", name, usb
+    }
+  '
+}
--- .config/fish/functions/lsusb-speed.fish
+++ .config/fish/functions/lsusb-speed.fish
@@ -1,3 +1,13 @@
 function lsusb-speed --description='List protocol versions of connected USB devices'
-  lsusb -v 2> /dev/null | grep --color=never -G "bcdUSB\|Bus [[:digit:]]"
+  lsusb -v 2>/dev/null | awk '
+    /^Bus/ { name=$0 }
+    /bcdUSB/ {
+      usb=$0
+      sub(/.*bcdUSB[[:space:]]+/,"",usb)
+      sub(/^Bus [0-9]+ Device [0-9]+: ID [0-9a-f]+:[0-9a-f]+ /,"",name)
+      sub(/[[:space:]]+$/, "", name)
+      if (length(name) > 68) name=substr(name,1,65)"…"
+      printf "%-68s %s\n", name, usb
+    }
+  '
 end
