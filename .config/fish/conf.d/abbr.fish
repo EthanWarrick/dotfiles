106b954e2c411f8baebc29c5f6b8e7459e937da3
Ethan Warrick <ewboy12@gmail.com>
2025-10-29 17:07:00 -0500

    fish: network drive abbreviation
    This makes //<path> replace with /mnt/<path>. This mimics windows
    network paths that start with '\\'.

--- .config/fish/conf.d/abbr.fish
+++ .config/fish/conf.d/abbr.fish
@@ -7,3 +7,18 @@ abbr --add dotdot --regex '^\.\.+$' --function multicd
 abbr --add n nvim
 abbr --add lzg lazygit
 abbr --add lzd lazydocker
+
+
+function __convert_slashes
+    for arg in $argv
+        if test (string match -r '^//' $arg)
+            set -l temp (string replace -r '^//' '/mnt/' $arg)
+            if test -e $temp
+                echo $temp
+                continue
+            end
+        end
+        echo $arg
+    end
+end
+abbr --add slashes --position anywhere --regex '^//.*' --function __convert_slashes
